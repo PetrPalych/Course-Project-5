3c9935fb923010677734fab31389a856bb9562b8
bekturmsv <itsbektur@gmail.com>
2023-12-14 01:36:02 +0600

    provide data to components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UilReact from "@iconscout/react-unicons/icons/uil-react";
 import TopButtons from "./components/TopButtons";
 import Inputs from "./components/Inputs";
@@ -8,19 +8,30 @@ import Forecast from "./components/Forecast";
 import getFormattedWeatherData from "./services/weatherService";
 
 const App = () => {
-  const fetchWeather = async () => {
-    const data = await getFormattedWeatherData({ q: "london" });
-    console.log(data);
-  };
-  fetchWeather();
+  const [query, setQuery] = useState({ q: "osh" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormattedWeatherData({ ...query, ...units }).then((data) => {
+        setWeather(data);
+      });
+    };
+    fetchWeather();
+  }, [query, units]);
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
-      <TopButtons />
-      <Inputs />
-      <TimeAndLocation />
-      <TemperatureAndDetails />
-      <Forecast title="hourly forecast" />
-      <Forecast title="daily forecast" />
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-28 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons setQuery={setQuery} />
+      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
+      {weather && (
+        <div>
+          <TimeAndLocation weather={weather} />
+          <TemperatureAndDetails weather={weather} />
+          <Forecast title="hourly forecast" items={weather.hourly} />
+          <Forecast title="daily forecast" items={weather.daily} />
+        </div>
+      )}
     </div>
   );
 };
